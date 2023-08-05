d84abe8bf4b97c4b6ef9c8beb91a593b1fda758f
Samuel Amankwaa-Frempong <samankwaa.frempong@gmail.com>
2023-08-05 12:39:34 +0000

    Update Search.js with lesson on Linear Search

--- Arrays/Lessons/Search.js
+++ Arrays/Lessons/Search.js
@@ -1,1 +1,21 @@
+const array = [1, 2, 3, 4, 5];
 
+// linear searching -> go through all elements o(n)
+//find 4 from the array and return its index
+
+const linearSearch = (arr, target) => {
+  let foundNum;
+  let foundIndex;
+  array.forEach((element, indx) => {
+    if(element === target){
+      foundNum = element
+      foundIndex = indx
+    }
+  })
+  if(!foundNum){
+    return target + ' was not found'
+  }
+  return 'We found ' + foundNum + ' at index ' + foundIndex
+}
+
+console.log(linearSearch(array, 3))
