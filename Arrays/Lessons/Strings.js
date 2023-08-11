04728797de536657233ea0a9daa79d0920d1083e
Samuel Amankwaa-Frempong <samankwaa.frempong@gmail.com>
2023-08-11 23:40:45 +0000

    Update Strings.js with lessons on string manipulation

--- Arrays/Lessons/Strings.js
+++ Arrays/Lessons/Strings.js
@@ -1,1 +1,8 @@
+let string = 'This is a good sentence';
 
+let alteredString = string.split(' ')
+let newArr = alteredString.join(',')
+
+console.log(alteredString)
+console.log(newArr)
+console.log(typeof newArr)
